059fe89ccfd18d1a74e73a4e8f8fc572d9a42188
Cindy Ren <bdcindy78@gmail.com>
2023-06-21 17:18:35 +1000

    add mouseevent and delete button

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,8 +5,13 @@ import './index.css';
 export default class index extends Component {
   handleKeyUp = (event) => {
     if (event.keyCode !== 13) return;
+    if (event.target.value.trim() === '') {
+      alert('输入不能为空');
+      return;
+    }
     console.log(event.target.value);
     this.props.addItem({ id: nanoid(), name: event.target.value, done: false });
+    event.target.value = '';
   };
   render() {
     return (
@@ -14,7 +19,7 @@ export default class index extends Component {
         <input
           onKeyUp={this.handleKeyUp}
           type="text"
-          placeholder="请输入你的任务名称，按回车键确认0000"
+          placeholder="请输入你的任务名称，按回车键确认"
         />
       </div>
     );
